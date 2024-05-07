528b857cd956d603a5c20fbae895df0e31696023
Sohyunnnn <thgus05061@naver.com>
2024-05-08 02:05:30 +0900

    feat: 로딩 스피너 구현 완료

--- week3/MoviePoster/src/pages/NowPlayingPage.jsx
+++ week3/MoviePoster/src/pages/NowPlayingPage.jsx
@@ -1,16 +1,19 @@
 import Info from '../components/info';
 import { nowPlaytingMovies } from '../api/nowPlayingMovies';
 import { useEffect, useState } from 'react';
+import LoadingSpinner from '../components/LoadingSpinner';
 
 
 const NowPlayingPage = () => {
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true); 
 
   useEffect(() => {
     const getNowPlayingMovies = async () => {
       try {
         const data = await nowPlaytingMovies();
         setMovies(data.results);
+        setLoading(false);
       } catch (error) {
         console.error(error);
       }
@@ -20,14 +23,18 @@ const NowPlayingPage = () => {
   }, []);
 
   return (
-    <main style={{margin: '30px 0'}}>
-      <div className="moviesGrid">
-        {movies.map(movie => (
-          <div key={movie.id} className="movieItem">
-            <Info movie={movie} /> 
-          </div>
-        ))}
-      </div>
+    <main style={{margin: '30px auto'}}>
+      {loading ? (
+       <LoadingSpinner />
+      ) : (
+        <div className="moviesGrid">
+          {movies.map(movie => (
+            <div key={movie.id} className="movieItem">
+              <Info movie={movie} />
+            </div>
+          ))}
+        </div>
+      )}
     </main>
   );
 }
--- week3/MoviePoster/src/pages/PopularPage.jsx
+++ week3/MoviePoster/src/pages/PopularPage.jsx
@@ -1,16 +1,19 @@
 import Info from '../components/info';
 import { fetchMovies } from '../api/movies';
 import { useEffect, useState } from 'react';
+import LoadingSpinner from '../components/LoadingSpinner';
 
 const PopularPage = () => {
 
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true); 
 
   useEffect(() => {
     const fetchMovieData = async () => {
       try {
         const data = await fetchMovies();
         setMovies(data.results);
+        setLoading(false);
       } catch (error) {
         console.error(error);
       }
@@ -20,14 +23,18 @@ const PopularPage = () => {
   }, []);
 
   return (
-    <main style={{margin: '30px 0'}}>
-      <div className="moviesGrid">
-        {movies.map(movie => (
-          <div key={movie.id} className="movieItem">
-            <Info movie={movie} /> 
-          </div>
-        ))}
-      </div>
+    <main style={{margin: '30px auto'}}>
+      {loading ? (
+       <LoadingSpinner />
+      ) : (
+        <div className="moviesGrid">
+          {movies.map(movie => (
+            <div key={movie.id} className="movieItem">
+              <Info movie={movie} />
+            </div>
+          ))}
+        </div>
+      )}
     </main>
   );
 }
