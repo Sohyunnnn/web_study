b793c5ea16770f24887797e65c1ea895448814c2
Sohyunnnn <thgus05061@naver.com>
2024-05-07 20:44:19 +0900

    feat: api 연결

--- week3/MoviePoster/src/App.jsx
+++ week3/MoviePoster/src/App.jsx
@@ -1,20 +1,34 @@
 import './App.css'
 import Info from '../src/components/info'
-import { movies } from '../src/api/movies';
+import { fetchMovies } from '../src/api/movies';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovieData = async () => {
+      try {
+        const data = await fetchMovies();
+        setMovies(data.results);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchMovieData();
+  }, []);
 
   return (
-    <>
-       <div className="moviesGrid">
-        {movies.results.map(movie => (
+    <div className="moviesGrid">
+      {movies.map(movie => (
         <div key={movie.id} className="movieItem">
           <Info movie={movie} /> 
         </div>
-    ))}
-  </div>
-    </>
-  )
+      ))}
+    </div>
+  );
 }
+
 
 export default App
