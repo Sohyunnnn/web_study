bb67e36aea3c05f4236769ef9a49bbbfee0a79fe
Sohyunnnn <thgus05061@naver.com>
2024-05-07 20:45:17 +0900

    feat: ui 문제 해결 및 styled-components 설정

--- week3/MoviePoster/src/components/info.jsx
+++ week3/MoviePoster/src/components/info.jsx
@@ -2,55 +2,65 @@
 //import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-
-const InfoBox = styled.div`
-  background-color: rgb(0, 0, 68);
-  width: 600px;
-
-  &:hover {
-    opacity: 0.2;
-  }
-`;
+import { IMG_BASE_URL } from '../api/config';
 
 const InfoImg = styled.img`
-  margin: 50px 0 20px 0;
+  margin: 20px 0 10px 0;
 `;
 
 const Overview = styled.p`
-  position: absolute; 
-  display: none; 
-  padding: 20px; 
-  color: white;
-  width: 550px;
-  z-index: 1;
+  position: absolute;
+  display: none;
   padding: 20px;
-  font-size: 35px;
+  color: white;
+  width: 210px;
+  z-index: 1;
+  font-size: 13px;
+  top: 0; /* 부모 요소에 대해 상단으로 고정 */
+  left: 0; /* 부모 요소에 대해 왼쪽으로 고정 */
 `;
 
 const Overlap = styled.div`
   display: none; 
   background-color: black;
-  height: 1020px;
-  width: 600px;
+  height: 500px;
+  width: 250px;
   opacity: 0.8;
   position: absolute; 
   z-index: 0;
+  top: 0; 
+  left: 0; 
 `;
 
 const Review = styled.p`
-  padding: 0 0 30px 0;
+  padding: 0 0 10px 0;
 `;
 
 const Title = styled.h2`
-  font-size: 30px;
+  font-size: 20px;
+  width: 200px;
+  margin: 0 auto;
 `;
+
+const InfoBox = styled.div`
+  background-color: rgb(0, 0, 68);
+  width: 250px;
+  height: 500px;
+  position: relative;
+  &:hover {
+    ${Overlap}, ${Overview} {
+      display: block; /* 호버 상태일 때 나타남 */
+    }
+  }
+`;
+
 
 const Info = ({ movie }) => {
   return (
     <InfoBox>
       <Overview>{movie.overview}</Overview>
       <Overlap />
-      <InfoImg src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
+      <InfoImg src={`${IMG_BASE_URL}${movie.poster_path}`} alt={movie.title} />
       <Title>{movie.title}</Title>
       <p>{movie.release_date}</p>
       <Review>평점: {movie.vote_average} / 10</Review>
