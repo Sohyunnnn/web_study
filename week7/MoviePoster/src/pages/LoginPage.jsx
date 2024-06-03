0477932977ac0f1da77071e66c9e17f7e7d0bee4
Sohyunnnn <thgus05061@naver.com>
2024-06-04 02:59:34 +0900

    feat: 로그인 시 검증 수정 및 ui 수정

--- week7/MoviePoster/src/pages/LoginPage.jsx
+++ week7/MoviePoster/src/pages/LoginPage.jsx
@@ -12,18 +12,20 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  & > form > * {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 const ErrorMessage = styled.p`
   color: red;
   margin: 5px 0 7px 0;
   font-size: 12px;
+  text-align: center;
 `;
 
 const Button = styled.button`
@@ -48,6 +50,11 @@ const LoginPage = () => {
     setter(e.target.value);
     setErrors((prevErrors) => ({ ...prevErrors, [field]: '' }));
     validateForm(); // 입력값이 변경될 때마다 검증 함수 호출
+    
+    if (field === 'password') {
+      validatePassword(e.target.value);
+    }
+
   };
 
   const handleSubmit = (e) => {
@@ -56,26 +63,29 @@ const LoginPage = () => {
     // Add your login logic here
   };
 
+  const validatePassword = (password) => {
+    if (!password) return 'Password is required';
+    if (typeof password !== 'string') return 'Password must be a string';
+    if (password.length < 4) return 'Password must be at least 4 characters long';
+    if (password.length > 12) return 'Password must be no more than 12 characters long';
+    // const hasLetter = /[a-zA-Z]/.test(password);
+    // const hasNumber = /[0-9]/.test(password);
+    // const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    // if (!hasLetter || !hasNumber || !hasSpecialChar) return 'Password must include letters, numbers, and special characters';
+    return '';
+  };
+
   // 검증 함수
   const validateForm = () => {
     const newErrors = {};
     if (!id) newErrors.id = 'Please enter your id';
-    if (!password) {
-      newErrors.password = 'Password is required';
-    } else if (typeof password !== 'string') {
-      newErrors.password = 'Password must be a string';
-    } else if (password.length < 4) {
-      newErrors.password = 'Password must be at least 4 characters long';
-    } else if (password.length > 12) {
-      newErrors.password = 'Password must be no more than 12 characters long';
-    } else {
-      const hasLetter = /[a-zA-Z]/.test(password);
-      const hasNumber = /[0-9]/.test(password);
-      const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
-      if (!hasLetter || !hasNumber || !hasSpecialChar) {
-        newErrors.password = 'Password must include letters, numbers, and special characters';
-      }
-    }
+
+    // 패스워드 필드 검증
+  const passwordError = validatePassword(password);
+  if (passwordError) {
+    newErrors.password = passwordError;
+  }
+
     setErrors(newErrors);
     setIsFormValid(Object.keys(newErrors).length === 0);
   };
@@ -86,7 +96,7 @@ const LoginPage = () => {
     <main>
       <Container>
         <h1>Login</h1>
-        <form onSubmit={handleSubmit}>
+        <Form onSubmit={handleSubmit}>
         <Input
             placeholder='Please enter your id'
             value={id}
@@ -101,7 +111,7 @@ const LoginPage = () => {
           />
           {errors.password && <ErrorMessage>{errors.password}</ErrorMessage>}
           <Button type='submit' disabled={!isFormValid} onClick={validateForm}>Login</Button>
-        </form>
+        </Form>
       </Container>
     </main>
   )
